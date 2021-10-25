9cf6bec6a718e9a4c0c98ba19710f057bcf25fd6
Sergey Ratiashvili <sergey.ratiashvili@aggregion.com>
2021-10-25 13:44:41 +0300

    feat(encoder) added progress and some layout

--- src/Encoder/index.tsx
+++ src/Encoder/index.tsx
@@ -14,11 +14,11 @@ import {
   BLOCKS_COUNT,
   CHUNK_SIZE,
   COMPRESS_PAYLOAD,
-  DEBUG_IMAGES,
   EXTRA_BLOCKS_COUNT,
   IMAGE_SIZE,
 } from "../config/coding";
-import { flushSync } from "react-dom";
+import asyncPool from "tiny-async-pool";
+import "./style.css";
 
 const qrHints = new Map();
 const qrEncoder = new BrowserQRCodeSvgWriter();
@@ -28,39 +28,38 @@ const createQR = (
   total: number,
   totalPlain: number,
   chunk: Uint8Array
-) => {
-  const header = Buffer.from(
-    new Uint16Array([total, totalPlain, index]).buffer
-  );
-  const text = Buffer.concat([header, chunk]);
-  if (index === 129) console.log(text, chunk);
-
-  const svg = qrEncoder.write(
-    text.toString("base64"),
-    IMAGE_SIZE,
-    IMAGE_SIZE,
-    qrHints
-  );
-  const canvas = document.createElement("canvas");
-  const ctx = canvas.getContext("2d");
-  if (!ctx) throw new Error("no ctx");
-  canvas.width = IMAGE_SIZE;
-  canvas.height = IMAGE_SIZE;
-  ctx.fillStyle = "#fff";
-  ctx.fillRect(0, 0, IMAGE_SIZE, IMAGE_SIZE);
-  ctx.fillStyle = "#000";
-  for (const rect of svg.getElementsByTagName("rect")) {
-    ctx.fillRect(
-      parseInt(rect.getAttribute("x") || "0"),
-      parseInt(rect.getAttribute("y") || "0"),
-      parseInt(rect.getAttribute("width") || "0"),
-      parseInt(rect.getAttribute("height") || "0")
+): Promise<string> =>
+  new Promise((r) => {
+    const header = Buffer.from(
+      new Uint16Array([total, totalPlain, index]).buffer
     );
-  }
-  const image = canvas.toDataURL();
-  canvas.remove();
-  return image;
-};
+    const text = Buffer.concat([header, chunk]);
+    const svg = qrEncoder.write(
+      text.toString("base64"),
+      IMAGE_SIZE,
+      IMAGE_SIZE,
+      qrHints
+    );
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    if (!ctx) throw new Error("no ctx");
+    canvas.width = IMAGE_SIZE;
+    canvas.height = IMAGE_SIZE;
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, IMAGE_SIZE, IMAGE_SIZE);
+    ctx.fillStyle = "#000";
+    for (const rect of svg.getElementsByTagName("rect")) {
+      ctx.fillRect(
+        parseInt(rect.getAttribute("x") || "0"),
+        parseInt(rect.getAttribute("y") || "0"),
+        parseInt(rect.getAttribute("width") || "0"),
+        parseInt(rect.getAttribute("height") || "0")
+      );
+    }
+    const image = canvas.toDataURL();
+    canvas.remove();
+    setImmediate(() => r(image));
+  });
 
 const compressPayload = (payload: string): Uint8Array => {
   return COMPRESS_PAYLOAD ? compress(payload) : Buffer.from(payload);
@@ -106,39 +105,49 @@ function Encoder() {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const [log, setLog] = useState<string[]>([]);
   const [frameDuration, setFrameDuration] = useState<number>(2);
+  const [gifProgress, setGifProgress] = useState<number | undefined>();
+  const [resultImage, setResultImage] = useState<
+    | {
+        name: string;
+        src: string;
+      }
+    | undefined
+  >();
 
-  const addLog = (s: string): void => {
+  const addLog = async (s: string): Promise<void> => {
     setLog((log) => [...log, s]);
+    await new Promise<void>((r) => setTimeout(() => r(), 0));
   };
 
   const encode = async () => {
     if (!textareaRef.current) return;
-    const container = document.getElementById("imageBuffer");
+    setGifProgress(0);
+    setLog([]);
+    setResultImage(undefined);
     const payload = (textareaRef.current as HTMLTextAreaElement).value;
     if (!payload) return;
-    addLog(`Payload length: ${payload.length}`);
+    await addLog(`Payload length: ${payload.length}`);
     const compressedPayload = compressPayload(payload);
-    addLog(`Compressed length: ${compressedPayload.length}`);
-    // await new Promise<void>((r) => setTimeout(() => r(), 0));
+    await addLog(`Compressed length: ${compressedPayload.length}`);
+    setGifProgress(2);
     // const decompressed = decompress(stringToUint8Array(compressedPayload));
     const parts = stringToChunks(compressedPayload, CHUNK_SIZE);
+    setGifProgress(5);
     const chunks = solmonReedChunks(parts);
-    console.log("CHUNKS>", chunks.length);
-    console.log(chunks.map((c) => c.length));
-    addLog(`Chunks count ${chunks.length}`);
-    const images = [];
-    for (let i = 0; i < chunks.length; i++) {
-      images.push(createQR(i, chunks.length, parts.length, chunks[i]));
-    }
+    await addLog(`Chunks count ${chunks.length}`);
+    await addLog(`Creating qrs...`);
+    setGifProgress(10);
 
-    addLog(`Creating GIF...`);
-    // DEBUG_IMAGES &&
-    //   images.forEach((image) => {
-    //     const i = new Image();
-    //     i.src = image;
-    //     container?.appendChild(i);
-    //   });
-    console.log(images);
+    const images = await asyncPool(
+      10,
+      chunks.map((chunk, index) => ({ data: chunk, index })),
+      (chunk) => {
+        return createQR(chunk.index, chunks.length, parts.length, chunk.data);
+      }
+    );
+    setGifProgress(20);
+
+    await addLog(`Creating GIF...`);
     gifshot.createGIF(
       {
         images: images,
@@ -146,21 +155,14 @@ function Encoder() {
         gifHeight: IMAGE_SIZE,
         numWorkers: 3,
         progressCallback: function (captureProgress: any) {
-          console.log(captureProgress);
+          setGifProgress((captureProgress * 80 + 20) | 0);
         },
         frameDuration,
       },
       function (obj: { error: any; image: any }) {
         if (!obj.error) {
-          if (!container) return;
-          // container.innerHTML = "";
-          const image = composeImage(obj.image);
-          const generated = Date.now();
-          image.onclick = () => {
-            generateDownload(`${generated}.gif`, obj.image, FileType.GIF);
-          };
-          image.style.cursor = "pointer";
-          container.appendChild(image);
+          setGifProgress(undefined);
+          setResultImage({ src: obj.image, name: `${Date.now()}.gif` });
         }
       }
     );
@@ -200,24 +202,44 @@ function Encoder() {
                 </div>
               )}
             </Dropzone>
-            <div className="columns">
-              <div className="column">
-                <input
-                  className="button is-primary"
-                  value="Encode"
-                  onChange={() => {}}
-                  onClick={() => encode()}
-                />
-              </div>
+            <div>
+              <input
+                className="button is-primary"
+                value="Encode"
+                onChange={() => {}}
+                onClick={() => encode()}
+              />
             </div>
-            <div className="columns is-centered">
-              <div id="imageBuffer" className="column is-half"></div>
+            <div className="column">
+              {gifProgress && (
+                <progress className="progress" value={gifProgress} max="100">
+                  {gifProgress}%
+                </progress>
+              )}
+              {gifProgress &&
+                log.map((item, index) => (
+                  <div key={`log-${index}`}>{item}</div>
+                ))}
             </div>
           </div>
-          <div className="column">
-            {log.map((item, index) => (
-              <div key={`log-${index}`}>{item}</div>
-            ))}
+
+          <div className="column result">
+            {resultImage && (
+              <img
+                src={resultImage.src}
+                width={500}
+                height={500}
+                alt="qr"
+                className="resultImage"
+                onClick={() => {
+                  generateDownload(
+                    resultImage.name,
+                    resultImage.src,
+                    FileType.GIF
+                  );
+                }}
+              />
+            )}
           </div>
         </div>
       </div>
